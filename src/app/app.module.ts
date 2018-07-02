28040ea6c7870d24ac9c20bcc9b3162db1c08d83
sandeep1456 <kamblesandeepr@gmail.com>
2018-07-02 13:06:47 +0530

    Angular App initial files and Resume Preview with static data

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ResumePreviewComponent } from './resume-preview/resume-preview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResumePreviewComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
